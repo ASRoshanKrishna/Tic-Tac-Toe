d2249a29db5142088a2665e03c8aaa92a563352e
Roshan Krishna A.S <asroshank@gmail.com>
2024-06-04 19:03:48 +0530

    func createUser and dialog form

--- myScript.js
+++ myScript.js
@@ -26,13 +26,18 @@ const displayController = (function() {
     return {dis};
 })();
 
-displayController.dis();
+
+const dialog = document.querySelector("dialog");
+const form = document.querySelector('#fo');
+dialog.showModal();
+
 
 let indexBox = 0;
 let over = 0;
+let ros = "";
+let kris = "";
 
-
-const user1 = function(name) {
+const user1 = function(name1) {
     const { arrayBoard } = gameBoard;
     const arr1x = [0, 0, 0];
     const arr1y = [0, 0, 0];
@@ -52,12 +57,14 @@ const user1 = function(name) {
             }
         }
     }
+    const getarr1x = () => arr1x;
+    const getarr1y = () => arr1y;
     const getDiag1 = () => diag1;
     const getrevDiag1 = () => revDiag1;
-    return {name, createX, arr1x, arr1y, getDiag1, getrevDiag1};
+    return {name1, createX, getarr1x, getarr1y, getDiag1, getrevDiag1};
 }
 
-const user2 = function(name) {
+const user2 = function(name2) {
     const { arrayBoard } = gameBoard;
     const arr2x = [0, 0, 0];
     const arr2y = [0, 0, 0];
@@ -77,19 +84,35 @@ const user2 = function(name) {
             }
         }
     }
+    const getarr2x = () => arr2x;
+    const getarr2y = () => arr2y;
     const getDiag2 = () => diag2;
     const getrevDiag2 = () => revDiag2;
-    return {name, createO, arr2x, arr2y, getDiag2, getrevDiag2};
+    return {name2, createO, getarr2x, getarr2y, getDiag2, getrevDiag2};
 }
 
-const ros = user1("ros");
-const kris = user2("kris");
+const createUsers = function() {
+    const p1 = document.getElementById("player1");
+    const p2 = document.getElementById("player2");
+    ros = user1(p1.value);
+    kris = user2(p2.value);
+    return {ros, kris};
+};
+
+const add = function() {
+    event.preventDefault();
+    createUsers();
+    
+    form.reset();
+    dialog.close();
+}
+
 
 const game = function(xPos, yPos) {
     
 
-    const { arr1x, arr1y, getDiag1, getrevDiag1 } = ros;
-    const { arr2x, arr2y, getDiag2, getrevDiag2 } = kris;
+    const { name1, getarr1x, getarr1y, getDiag1, getrevDiag1 } = ros;
+    const { name2, getarr2x, getarr2y, getDiag2, getrevDiag2 } = kris;
     
     if(over!=1){
         if(indexBox%2==0){
@@ -106,18 +129,18 @@ const game = function(xPos, yPos) {
         displayController.dis();
         // console.log(ros.getDiag1(), kris.getDiag2(), ros.getrevDiag1(), kris.getrevDiag2());
         if(indexBox>3){
-            if((arr1x[0]==3 || arr1x[1]==3 || arr1x[2]==3) || (arr1y[0]==3 || arr1y[1]==3 || arr1y[2]==3) || ros.getDiag1()==3 || ros.getrevDiag1()==3){
-                console.log("ros won");
+            if((getarr1x()[0]==3 || getarr1x()[1]==3 || getarr1x()[2]==3) || (getarr1y()[0]==3 || getarr1y()[1]==3 || getarr1y()[2]==3) || getDiag1()==3 || getrevDiag1()==3){
+                console.log(`${name1} won`);
                 over = 1;
                 return;
             }
-            else if((arr2x[0]==3 || arr2x[1]==3 || arr2x[2]==3) || (arr2y[0]==3 || arr2y[1]==3 || arr2y[2]==3) || kris.getDiag2()==3 || kris.getrevDiag2()==3){
-                console.log("kris won");
+            else if((getarr2x()[0]==3 || getarr2x()[1]==3 || getarr2x()[2]==3) || (getarr2y()[0]==3 || getarr2y()[1]==3 || getarr2y()[2]==3) || getDiag2()==3 || getrevDiag2()==3){
+                console.log(`${name2} won`);
                 over = 1;
                 return;
             }
         }
-        if(indexBox==8){
+        if(indexBox==9){
             console.log("Tie");
             over = 1;
         }
