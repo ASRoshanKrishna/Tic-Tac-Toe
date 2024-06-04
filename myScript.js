6fed0cf1627765fdd20495dc5c4d78305b0fc321
Roshan Krishna A.S <asroshank@gmail.com>
2024-06-04 15:35:16 +0530

    display controller

--- myScript.js
+++ myScript.js
@@ -28,6 +28,10 @@ const displayController = (function() {
 
 displayController.dis();
 
+let indexBox = 0;
+let over = 0;
+
+
 const user1 = function(name) {
     const { arrayBoard } = gameBoard;
     const arr1x = [0, 0, 0];
@@ -36,6 +40,7 @@ const user1 = function(name) {
     let revDiag1 = 0;
     const createX = function(xPos,yPos) {
         if(arrayBoard[xPos][yPos] == " "){
+            indexBox++;
             arrayBoard[xPos][yPos] = "x";
             arr1x[xPos]++;
             arr1y[yPos]++;
@@ -45,12 +50,6 @@ const user1 = function(name) {
             if((xPos==1 && yPos==1) || (xPos==0 && yPos==2) || (xPos==2 && yPos==0)){
                 revDiag1++;
             }
-        }
-        else{
-            const pv = prompt("ros i j");
-            const a= pv.split(" ");
-            const ros = user1("ros");
-            ros.createX(a[0],a[1]);
         }
     }
     const getDiag1 = () => diag1;
@@ -66,6 +65,7 @@ const user2 = function(name) {
     let revDiag2 = 0;
     const createO = function(xPos,yPos) {
         if(arrayBoard[xPos][yPos] == " "){
+            indexBox++;
             arrayBoard[xPos][yPos] = "o";
             arr2x[xPos]++;
             arr2y[yPos]++;
@@ -76,53 +76,53 @@ const user2 = function(name) {
                 revDiag2++;
             }
         }
-        else{
-            const pv = prompt("kris i j");
-            const a = pv.split(" ");
-            const kris = user2("kris");
-            kris.createO(a[0],a[1]);
-        }
     }
     const getDiag2 = () => diag2;
     const getrevDiag2 = () => revDiag2;
     return {name, createO, arr2x, arr2y, getDiag2, getrevDiag2};
 }
 
-const game = function() {
-    const ros = user1("ros");
-    const kris = user2("kris");
+const ros = user1("ros");
+const kris = user2("kris");
+
+const game = function(xPos, yPos) {
+    
 
     const { arr1x, arr1y, getDiag1, getrevDiag1 } = ros;
     const { arr2x, arr2y, getDiag2, getrevDiag2 } = kris;
-
-
-    for(let i = 0;i<9;i++){
-        if(i%2==0){
-            const pv = prompt("ros i j");
-            const a= pv.split(" ");
-            ros.createX(a[0],a[1]);
+    
+    if(over!=1){
+        if(indexBox%2==0){
+            // const pv = prompt("ros i j");
+            // const a= pv.split(" ");
+            ros.createX(xPos, yPos);
         }
         else{
-            const pv = prompt("kris i j");
-            const a = pv.split(" ");
-            kris.createO(a[0],a[1]);
+            // const pv = prompt("kris i j");
+            // const a = pv.split(" ");
+            kris.createO(xPos, yPos);  
         }
         gameBoard.displayBoard();
         displayController.dis();
         // console.log(ros.getDiag1(), kris.getDiag2(), ros.getrevDiag1(), kris.getrevDiag2());
-        if(i>3){
+        if(indexBox>3){
             if((arr1x[0]==3 || arr1x[1]==3 || arr1x[2]==3) || (arr1y[0]==3 || arr1y[1]==3 || arr1y[2]==3) || ros.getDiag1()==3 || ros.getrevDiag1()==3){
                 console.log("ros won");
+                over = 1;
                 return;
             }
             else if((arr2x[0]==3 || arr2x[1]==3 || arr2x[2]==3) || (arr2y[0]==3 || arr2y[1]==3 || arr2y[2]==3) || kris.getDiag2()==3 || kris.getrevDiag2()==3){
                 console.log("kris won");
+                over = 1;
                 return;
             }
         }
-        if(i==8)
-        console.log("Tie");
+        if(indexBox==8){
+            console.log("Tie");
+            over = 1;
+        }
     }
+    console.log(indexBox);
 }
 
 // gameBoard.createO(0,1);
@@ -131,4 +131,3 @@ const game = function() {
 
 gameBoard.displayBoard();
 displayController.dis();
-game();
